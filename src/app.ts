f5a91b5eb4cad340411018b3d4f9785b27528dec
Felipe Pereira <fehpereira237@gmail.com>
2026-02-11 21:21:42 -0300

    deploy2

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,16 @@ import { Server } from 'socket.io';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5500'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5500'],
     methods: ['GET', 'POST'],
   },
 });
